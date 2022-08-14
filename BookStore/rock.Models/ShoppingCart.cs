eb86ad030fe1e9075d0be1ae67e4bf4a77bb2592
olasubomi <olasubomiodekunle@gmail.com>
2022-08-14 21:52:53 +0100

    .

---
+++ BookStore/rock.Models/ShoppingCart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+
+namespace rock.Models
+{ public class ShoppingCart
+    {
+        
+        public Product product { get; set; }
+        [Range(1,1000,ErrorMessage = "Please enter  a value between 1 and 1000")]
+        public int Count { get; set; }
+        
+        
+    }
+}
+
